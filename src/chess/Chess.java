cde71eee3d24a3e608c3829136f775ec1825e1ac
JAKE_XU <neuromoments@163.com>
2022-04-09 15:32:13 +0800

    3:30

--- src/chess/Chess.java
+++ src/chess/Chess.java
@@ -1,6 +1,14 @@
 package chess;
 public class Chess {
     public static void main(String[] args) {
+        A a = A.cpp;
         
     }
+    static void out(A a){
+        if(a == A.cpp) System.out.println(123);
+    }
+}
+
+enum A{
+    java, cpp, python;
 }

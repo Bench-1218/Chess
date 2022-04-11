6cb1de36650452b4b97bc498ba679a7c8327aaaf
JAKE_XU <neuromoments@163.com>
2022-04-11 11:05:41 +0800

    debug

--- src/chess/debug/Debug.java
+++ src/chess/debug/Debug.java
@@ -1,13 +1,31 @@
 package chess.debug;
 
 import java.util.Scanner;
+import java.awt.Frame;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 import chess.core.board.Board;
 
 public class Debug {
     Board board = new Board();
+    public static final String WHITE_FG =  "\u001B[38;5;255m";
+    public static final String BLACK_FG = "\u001B[38;5;232m";
+    public static final String WHITE_BG = "\u001B[48;5;203m";
+    public static final String GRAY_BG = "\u001B[48;5;171m";
+    public static final String BLACK_BG = "\u001B[48;5;131m";
+    public static final String RESET = "\u001B[0m";
 
     public void run() {
+        rich();
+    }
+
+    public void rich(){
+        new KeyListener();
+    }
+
+    public void poor(){
+        // without UI
         Scanner sc = new Scanner(System.in);
         while(true){
             int code = sc.nextInt();
@@ -27,16 +45,58 @@ public class Debug {
 
 
     public void printBoard() {
+        printBoard(-1, -1);
+    }
+
+    public void printBoard(int x1, int y1){
         char[][] charBoard = board.getCharBoard();
         System.out.printf("round %d: %s\n", board.getRound(), board.getTurn());
         for(int y = 0; y < Board.HEIGHT; y++){
             for(int x = 0; x < Board.WIDTH; x++){
-                System.out.print(charBoard[x][y]);
+                String bg;
+                if((x+y)%2 == 0){
+                    bg = WHITE_BG;
+                }else{
+                    bg = BLACK_BG;
+                }
+                if(x == x1 && y == y1){
+                    bg = GRAY_BG;
+                }
+                System.out.print(bg + " ");
+                if(charBoard[x][y] == 'x'){
+                    System.out.print(' ');
+                }else{
+                    if(charBoard[x][y] >= 'a') System.out.print(BLACK_FG);
+                    else System.out.print(WHITE_FG);
+                    System.out.print(charBoard[x][y]);
+                }
+                System.out.print(bg + " " + RESET);
             }
             System.out.println();
         }
-        System.out.println();
+        System.out.println();       
     }
+
+    // private char char2pic(char c){
+    //     switch(c){
+    //         case 'B': case 'b':
+    //         return '♗';
+    //         case 'K': case 'k':
+    //         return '♔';
+    //         case 'N': case 'n':
+    //         return '♘';
+    //         case 'P': case 'p':
+    //         return '♙';
+    //         case 'Q': case 'q':
+    //         return '♕';
+    //         case 'R': case 'r':
+    //         return '♖';
+    //         case 'x':
+    //         return ' ';
+    //         default:
+    //         return ' ';
+    //     }
+    // }
     public void printAvailablePositions(int x, int y) {
         int[][] ps = board.getAvailablePositions(x, y);
         char[][] charBoard = board.getCharBoard();
@@ -52,11 +112,83 @@ public class Debug {
                             break;
                         }
                     }
-                    System.out.print(available ? ' ' : charBoard[x1][y1]);
+                    String bg;
+                    if((x1+y1)%2 == 0){
+                        bg = WHITE_BG;
+                    }else{
+                        bg = BLACK_BG;
+                    }
+                    if((x1 == x && y1 == y) || available){
+                        bg = GRAY_BG;
+                    }
+                    System.out.print(bg + " ");
+                    if(charBoard[x1][y1] == 'x'){
+                        System.out.print(' ');
+                    }else{
+                        if(charBoard[x1][y1] >= 'a') System.out.print(BLACK_FG);
+                        else System.out.print(WHITE_FG);
+                        System.out.print(charBoard[x1][y1]);
+                    }
+                    System.out.print(bg + " " + RESET);
                 }
                 System.out.println();
             }
         }
         System.out.println();
+    }
+
+    class KeyListener extends Frame{
+        private int x = 0;
+        private int y = 0;
+        private boolean select;
+        private int x1;
+        private int y1;
+        public KeyListener(){
+            this.setVisible(true);
+            this.setBounds(10,10,100,100);
+            this.addKeyListener(new KeyAdapter(){
+                @Override
+                public void keyPressed(KeyEvent e) {
+                    int keyCode = e.getKeyCode();
+                    switch(keyCode){
+                        case 87: // W
+                            if(y > 0){
+                                y -= 1;
+                                printBoard(x, y);
+                            }
+                            break;
+                        case 83: // S
+                            if(y < Board.HEIGHT - 1){
+                                y += 1;
+                                printBoard(x, y);
+                            }
+                            break;
+                        case 65: // A
+                            if(x > 0){
+                                x -= 1;
+                                printBoard(x, y);;
+                            }
+                            break;
+                        case 68: // D
+                            if(x < Board.WIDTH - 1){
+                                x += 1;
+                                printBoard(x, y);
+                            }
+                            break;
+                        case 74:
+                            if(!select){
+                                select = true;
+                                x1 = x;
+                                y1 = y;
+                                printAvailablePositions(x, y);
+                            }else{
+                                select = false;
+                                board.movePiece(x1, y1, x, y);
+                                printBoard(x, y);
+                            }
+                    }
+                }
+            });
+        }
     }
 }
